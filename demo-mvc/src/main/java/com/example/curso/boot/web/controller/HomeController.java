7c038f61d5415af8fe32ea8eb32ff572224885f9
jennifer.oliveira <jennifer.oliveira@leucotron.com.br>
2019-04-08 13:19:05 -0300

    iniciando o desenvolvimento do projeto: criando projeto, importando arquivos html, incluindo recursos webjars, e testes dos links de menu ja com bootstrap

---
+++ demo-mvc/src/main/java/com/example/curso/boot/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.curso.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author jennifer
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "/home";
+    }
+}
